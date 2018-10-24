ca7cb4f774d12069f8142841ac18f3e7a8f60b17
Mitchell Barfield <kmb0027@gmail.com>
2018-10-24 10:57:48 -0500

    Api Key is hidden. Can call and get response from web api. Converts recieved kelvin temperature to fahrenheit to display to user.

--- OpenWeatherMapProject/Program.cs
+++ OpenWeatherMapProject/Program.cs
@@ -2,6 +2,7 @@
 using System.Threading;
 using System.Net;
 using Newtonsoft.Json.Linq;
+using System.IO;
 
 namespace OpenWeatherMapProject
 {
@@ -9,10 +10,23 @@ namespace OpenWeatherMapProject
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What is the ZipCode of the area?");
+            int zipCode = int.Parse(Console.ReadLine());
+            Console.Clear();
+
             WebClient webClient = new WebClient();
-            string response = webClient.DownloadString("Https://api.chucknorris.io/jokes/random");
-            JObject jo = JObject.Parse(response);
-            Console.WriteLine(jo.GetValue("value"));
+#if DEBUG
+            string apiKey = File.ReadAllText("appsetting.development.txt");
+#else
+            string apiKey = File.ReadAllText("appsetting.release.txt");
+#endif
+            string fullApiLink = $"Https://api.openweathermap.org/data/2.5/weather?zip={zipCode}" + apiKey;
+            string ApiResponse = webClient.DownloadString(fullApiLink);
+            JObject jo = JObject.Parse(ApiResponse);
+            double tempMath = double.Parse(jo.GetValue("main")["temp"].ToString());
+            double FahrenheitTemp = tempMath * 9 / 5 - 459.67;
+            FahrenheitTemp = Math.Round(FahrenheitTemp, 1);
+            Console.WriteLine($"The temperature is currently {FahrenheitTemp} degrees fahrenheit.");
         }
     }
 }
