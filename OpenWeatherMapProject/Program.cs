36df09f2be9e6b7ad08f75b1372e37784dc1b675
Mitchell Barfield <kmb0027@gmail.com>
2018-10-24 09:17:33 -0500

    Initial set-up. Json nuget. Starting to write WebClient/DownloadString block.

---
+++ OpenWeatherMapProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace OpenWeatherMapProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient webClient = new WebClient();
+            string response = webClient.DownloadString("Https://api.chucknorris.io/jokes/random");
+            JObject jo = JObject.Parse(response);
+            Console.WriteLine(jo.GetValue("value"));
+        }
+    }
+}
